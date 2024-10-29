cc94c97a938547b2ace53905b0591a45bcb87b3d
Denni Karin <dekarin@taltech.ee>
2024-10-29 19:04:07 +0200

    init project

---
+++ src/test/java/com/spectre/numberservice/NumberserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spectre.numberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
